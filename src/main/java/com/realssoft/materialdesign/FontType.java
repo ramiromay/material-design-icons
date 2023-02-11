79ec0e474a2f2506e4a7378b34c3e2c76563dbc9
Ramiro May Jr <realssoft@gmail.com>
2023-02-10 20:02:51 -0600

    Solucion error

--- src/main/java/com/realssoft/materialdesign/FontType.java
+++ src/main/java/com/realssoft/materialdesign/FontType.java
@@ -3,9 +3,9 @@ package com.realssoft.materialdesign;
 public enum FontType
 {
 
-    MATERIAL_SYMBOLS_OUTLINED("font/MaterialSymbolsOutlined.ttf"),
-    MATERIAL_SYMBOLS_SHARP("font/MaterialSymbolsSharp.ttf"),
-    MATERIAL_SYMBOLS_ROUNDED("font/MaterialSymbolsRounded.ttf");
+    MATERIAL_SYMBOLS_OUTLINED("/font/MaterialSymbolsOutlined.ttf"),
+    MATERIAL_SYMBOLS_SHARP("/font/MaterialSymbolsSharp.ttf"),
+    MATERIAL_SYMBOLS_ROUNDED("/font/MaterialSymbolsRounded.ttf");
 
     private final String path;
 
